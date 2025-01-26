db9f8a6e7fd9485f9427fd9595448c95e8486f06
Benoit Thery <thery.benoit@hotmail.fr>
2025-01-26 13:32:59 +0100

    Routage WIP / Premiers composants

---
+++ src/Pages/Error.jsx
@@ -0,0 +1,12 @@
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+function Error () {
+    return (<div>
+        <Header />
+        <h1>Ceci est une page pour les erreurs</h1>
+        <Footer />
+    </div>)
+}
+
+export default Error
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+function Home () {
+    return (<div>
+        <Header />
+        <h1>Ceci est la page Home</h1>
+        <Footer />
+    </div>)
+}
+
+export default Home
