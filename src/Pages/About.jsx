18f7ba9c93cc59d39d709b93988f431eea29153c
Benoit Thery <thery.benoit@hotmail.fr>
2025-01-31 13:32:03 +0100

    Page About WIP

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -2,11 +2,18 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Banner from '../components/Banner';
 import banner from '../assets/Banner-about.png'
+import TextWrapper from '../components/TextWrapper';
 
 function About () {
     return (<div>
         <Header />
         <Banner text='' img={banner}/>
+        <section>
+            <TextWrapper text="Fiabilité"/>
+            <TextWrapper text="Respect"/>
+            <TextWrapper text="Service"/>
+            <TextWrapper text="Sécurité"/>
+        </section>
         <Footer />
     </div>)
 }
