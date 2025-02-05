0456819e1c0ae3f96a1d4ca484d5bd366a739db9
Benoit Thery <thery.benoit@hotmail.fr>
2025-02-05 11:44:02 +0100

    Page Logements WIP

--- src/Pages/Logement.jsx
+++ src/Pages/Logement.jsx
@@ -2,15 +2,40 @@ import Slideshow from '../components/Slideshow';
 import { useParams } from 'react-router-dom';
 import { useContext } from 'react'
 import { DataContext } from '../context/DataProvider'
+import TextWrapper from '../components/TextWrapper';
+import Tags from '../components/Tags';
 
 function Logement () {
 
-    const offer = useParams()
-    const data = useContext(DataContext)
+    const reqId = useParams()                                                           // L'ID du logement demandé
+    const data = useContext(DataContext)                                                // L'ensemble des données stockées dans le contexte
+    const offer = data.find(data => data.id === reqId.offerId)                          // On récupère les données du logement dans le contexte à partir de l'ID
 
-    return (<div>
-        <Slideshow pictures={data[1].pictures}/>
-    </div>)
+    return (<main>
+        <Slideshow pictures={offer.pictures}/>
+        <section className='offer'>
+            <div>
+                <div>
+                    <h1>{offer.title}</h1>
+                    <p>{offer.location}</p>
+                </div>
+                <div className='host'>
+                    <p>{offer.host.name}</p>
+                    <img src={offer.host.picture} />
+                </div>
+            </div>
+            <div>
+                <Tags tags={offer.tags} />
+                <div>
+                    {offer.rating}
+                </div>
+            </div>
+        </section>
+        <section className='wrapper_offer'>
+            <TextWrapper text="Description" desc={offer.description}/>
+            <TextWrapper text="Equipements" desc={offer.equipments}/>
+        </section>
+    </main>)
 }
 
 export default Logement
--- src/context/DataProvider/index.jsx
+++ src/context/DataProvider/index.jsx
@@ -2,7 +2,6 @@ import { createContext, useState } from "react"
 import ads from "../../data/annonces.json"
 
 export const DataContext = createContext()
-
 export const DataProvider = ({ children }) => {
 
     const [offers, updateoffer] = useState(ads)
