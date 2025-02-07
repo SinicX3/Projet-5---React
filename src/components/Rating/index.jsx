29e7b5b92ffe3a69a35d4c2380b12a041b987455
Benoit Thery <thery.benoit@hotmail.fr>
2025-02-07 11:50:54 +0100

    Page Logements

--- src/components/Rating/index.jsx
+++ src/components/Rating/index.jsx
@@ -1,13 +1,17 @@
 import star from '../../assets/star-solid.svg'
+import nostar from '../../assets/star-regular.svg'
 
 function Rating (rating) {
 
  const ratingList = []
  for (let i = 0; i < rating.rating; i++) {
-   ratingList.push(<img src={star}/>)
+  ratingList.push(<img src={star}/>)
  }
 
- console.log(ratingList)
+ const badRating = 5 % rating.rating
+ for (let i = 0; i < badRating; i++) {
+  ratingList.push(<img src={nostar}/>)
+ }
 
  return(<div className='rating'>
   {ratingList}
--- src/components/Slideshow/index.jsx
+++ src/components/Slideshow/index.jsx
@@ -8,16 +8,18 @@ function Slideshow ({ pictures }) {
 
     const NextPic = () => {
         if (index < (pictures.length-1)) {(updateIndex(index + 1))}
+        if (index === (pictures.length-1)) {(updateIndex(0))}
     }
     const PrevPic = () => {
         if (index > 0) {(updateIndex(index - 1))}
+        if (index === 0) {(updateIndex(pictures.length-1))}
     }
 
         return (<section>
             <div className="slideshow">
-                <div className={`icons ${(pictures.length > 1) ? 'active' : ''}`} >
-                    <img src={iconL} className='iconL' onClick={PrevPic}/>
-                    <img src={iconR} className='iconR' onClick={NextPic}/>
+                <div className={`icons ${(pictures.length > 0) ? 'active' : ''}`} >
+                    <img src={iconL} className={`iconL ${(pictures.length > 1) ? 'active' : 'none'}`} onClick={PrevPic}/>
+                    <img src={iconR} className={`iconR ${(pictures.length > 1) ? 'active' : 'none'}`} onClick={NextPic}/>
                     <p>{index + 1}/{pictures.length}</p>
                     <img src={pictures[index]} className='pic'/>
                 </div>
